32cf45103e833709e5d460882a4ed0a327c8fdd4
Bryson Creighton <brysoncreighton@gmail.com>
2020-06-24 12:04:18 -0600

    Initial server setup

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,9 @@
 const app = require('../src/app')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200 containing "Hello, bookmarks!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, boilerplate!')
+      .expect(200, 'Hello, bookmarks!')
   })
 })
