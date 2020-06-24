e77cda6db73f89094130afc6143343c1abd27f2a
Bryson Creighton <brysoncreighton@gmail.com>
2020-06-24 12:06:16 -0600

    Establish tabel seed data

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,122 @@
+INSERT INTO bookmarkTable (title, url, description, rating)
+  VALUES
+    (
+      'Google',
+      'http://www.google.com',
+      'Internet-related services and products.',
+      '3'
+    ),
+    (
+      'Thinkful',
+      'http://www.thinkful.com',
+      '1-on-1 learning to accelerate your way to a new high-growth tech career!',
+      '5'
+    ),
+    (
+      'Github',
+      'http://www.github.com',
+      'brings together the world''s largest community of developers.',
+      '4'
+    ),
+    (
+      'quis proident et qui nostrud',
+      'https://www.125.com',
+      'Culpa mollit id laboris do quis amet dolore non nisi elit nisi consequat enim. Dolor enim fugiat laborum reprehenderit et ea ea laboris voluptate non consequat.',
+      '1'
+    ),
+    (
+      'quis pariatur dolore excepteur nisi',
+      'https://www.126.com',
+      'Veniam ea do Lorem do veniam. Et esse excepteur nulla officia exercitation. Velit consequat cillum occaecat nostrud sit officia do consequat duis commodo ullamco aute sint magna. Pariatur consequat culpa amet veniam ullamco ea sunt non fugiat.',
+      '2'
+    ),
+    (
+      'nisi occaecat ipsum incididunt labore',
+      'https://www.127.com',
+      'Ea in sint occaecat cillum in qui magna laborum. Veniam irure ut incididunt veniam pariatur magna commodo. Duis ea duis aliqua do ut sunt commodo labore veniam deserunt ipsum voluptate.',
+      '5'
+    ),
+    (
+      'ut sit do voluptate ut',
+      'https://www.128.com',
+      'Laborum ad occaecat adipisicing culpa exercitation eu tempor. Ut ex commodo adipisicing laborum. In ea fugiat dolore consequat elit minim elit aliqua dolor enim.',
+      '3'
+    ),
+    (
+      'velit ad Lorem consequat amet',
+      'https://www.129.com',
+      'Irure ipsum tempor reprehenderit sint ut est. Minim officia deserunt reprehenderit labore.',
+      '4'
+    ),
+    (
+      'culpa aliqua Lorem ea sunt',
+      'https://www.130.com',
+      'Deserunt ipsum commodo cillum irure amet cillum amet id cupidatat deserunt ad irure culpa nostrud. Aliquip dolore esse laborum laboris sunt anim occaecat.',
+      '5'
+    ),
+    (
+      'laboris dolore id esse laborum',
+      'https://www.131.com',
+      'Non excepteur non cillum consequat pariatur ipsum culpa consequat sint anim cupidatat dolore. Ipsum est ut fugiat nisi minim eu minim aute enim sunt fugiat velit do. Enim ex et minim anim est laborum pariatur sunt nisi aliquip amet ullamco.\r\n',
+      '4'
+    ),
+    (
+      'exercitation labore tempor ea anim',
+      'https://www.132.com',
+      'Consequat ex adipisicing consectetur duis. Lorem pariatur Lorem in veniam amet anim elit nulla tempor. Consectetur nulla aliqua sunt voluptate irure non aute deserunt do nulla.',
+      '4'
+    ),
+    (
+      'pariatur minim pariatur fugiat consequat',
+      'https://www.133.com',
+      'Sit occaecat consectetur adipisicing esse et cillum exercitation ipsum esse anim pariatur adipisicing consectetur.',
+      '3'
+    ),
+    (
+      'Lorem ea ea esse in',
+      'https://www.134.com',
+      'Cupidatat eu pariatur minim est magna nulla qui ullamco deserunt Lorem est sint. Cillum excepteur incididunt aute eu tempor reprehenderit ex velit ea et deserunt.',
+      '4'
+    ),
+    (
+      'adipisicing sunt voluptate qui aliqua',
+      'https://www.135.com',
+      'Qui quis ut in pariatur Lorem velit laborum ipsum. Incididunt irure Lorem laborum nisi velit deserunt enim incididunt. Enim anim ex ex nostrud qui anim in tempor non.',
+      '4'
+    ),
+    (
+      'enim proident sit eu ea',
+      'https://www.136.com',
+      'Voluptate deserunt aute laboris veniam pariatur proident deserunt esse nostrud eu esse ullamco.',
+      '3'
+    ),
+    (
+      'ullamco enim amet quis laborum',
+      'https://www.137.com',
+      'Qui cupidatat quis sunt occaecat sunt id anim laboris commodo. Non nulla excepteur reprehenderit minim consectetur anim. Sunt laboris aliquip consectetur cupidatat elit.',
+      '1'
+    ),
+    (
+      'exercitation laboris irure id amet',
+      'https://www.138.com',
+      'Et amet dolor proident aliqua laboris nostrud officia elit exercitation qui nulla. Sunt dolor qui ut laborum est culpa officia nisi laborum magna sint esse.',
+      '1'
+    ),
+    (
+      'aliquip velit excepteur cillum qui',
+      'https://www.139.com',
+      'Adipisicing eiusmod exercitation ut ea amet. Consequat nisi minim id esse quis id elit incididunt elit fugiat dolor magna.',
+      '2'
+    ),
+    (
+      'aute proident laboris elit elit',
+      'https://www.140.com',
+      'Enim esse labore consectetur ipsum magna ad qui consequat pariatur adipisicing. Esse anim sunt id deserunt consectetur amet enim laboris elit ex quis et.',
+      '2'
+    ),
+    (
+      'mollit consectetur ut occaecat ut',
+      'https://www.124.com',
+      'Nostrud labore nisi ea magna. Quis sint occaecat enim quis occaecat enim esse. Ea sint eiusmod minim quis.',
+      '3'
+    );
